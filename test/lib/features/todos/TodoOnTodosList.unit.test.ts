1947fdc62a7e72223490147df17113e4918ce830
Rafał Majewski <goodheropl@gmail.com>
2022-12-05 02:27:31 +0100

    Uninstall @testing-library/jest-dom

--- test/lib/features/todos/TodoOnTodosList.unit.test.ts
+++ test/lib/features/todos/TodoOnTodosList.unit.test.ts
@@ -1,14 +1,13 @@
 import {render, screen} from "@testing-library/svelte";
-import matchers from "@testing-library/jest-dom/matchers";
 import {describe, test, expect} from "vitest";
-
-expect.extend(matchers);
 
 import {TodoOnTodosList} from "../../../../src/lib/features/todos/index.js";
 
 describe("TodoOnTodosList", () => {
 	test("renders", () => {
 		render(TodoOnTodosList, {todo: {id: 1, content: "My first todo", isCompleted: false}});
-		expect(screen.getByText("My first todo")).toBeInTheDocument();
+		expect(() => {
+			screen.getByText("My first todo");
+		}).not.toThrow();
 	});
 });
