607828aca40748e7237febde55c8189a13ff260d
Rafał Majewski <goodheropl@gmail.com>
2022-12-08 09:32:29 +0100

    Make the app fill the whole viewport height

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,21 @@
 <div class="svelte-app-root">
 	<slot />
 </div>
+
+<style lang="scss" global>
+	* {
+		box-sizing: border-box;
+	}
+	body {
+		margin: 0;
+	}
+	html {
+		min-height: 100%;
+	}
+	html,
+	body,
+	.sveltekit-body,
+	.svelte-app-root {
+		display: grid;
+	}
+</style>
