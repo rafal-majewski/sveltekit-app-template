1fbe07a3d529b454ad3f527f9fd084f2c2cc2fc2
Rafał Majewski <goodheropl@gmail.com>
2023-03-12 03:42:40 +0100

    Lowercase package names

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import {building} from "$app/environment";
 
 // Validate environment variables by importing appConfig
 if (!building) {
-	import("$lib/app-config/appConfig.js");
+	import("$lib/app_config/appConfig.js");
 }
 
 const handle: Handle = async ({event, resolve}) => {
