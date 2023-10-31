70c96e466880ae35e7f3154b7c0ce4e88ab55451
Rafał Majewski <goodheropl@gmail.com>
2023-10-31 23:51:22 +0100

    Test if Docker container requires an environment variable

--- test/dockerImage.unit.test.ts
+++ test/dockerImage.unit.test.ts
@@ -1,4 +1,4 @@
-import {describe, test} from "vitest";
+import {describe, expect, test} from "vitest";
 import * as testcontainers from "testcontainers";
 
 const dockerImageNodeVersion = "19.7";
@@ -22,4 +22,32 @@ describe("Docker container", () => {
 			.build();
 		await container.start();
 	}, 100000);
+	test('requires "WELCOME_MESSAGE" environment variable', async () => {
+		const container = await new testcontainers.GenericContainerBuilder(".", "Dockerfile")
+			.withBuildArgs({
+				"NODE_VERSION": dockerImageNodeVersion,
+			})
+			.build();
+		const startedContainer = await container.start();
+		const logs = await startedContainer.logs();
+		let doLogsContainWelcomeMessageEnvironmentVariableName = false;
+		let doLogsContainWordMissing = false;
+		logs.on("data", (chunk) => {
+			if (typeof chunk === "string") {
+				if (chunk.includes("WELCOME_MESSAGE")) {
+					doLogsContainWelcomeMessageEnvironmentVariableName = true;
+				}
+				if (chunk.toLowerCase().includes("missing")) {
+					doLogsContainWordMissing = true;
+				}
+			}
+		});
+		await new Promise<void>((resolve) => {
+			logs.on("end", () => {
+				resolve();
+			});
+		});
+		expect(doLogsContainWelcomeMessageEnvironmentVariableName).toBe(true);
+		expect(doLogsContainWordMissing).toBe(true);
+	});
 });
